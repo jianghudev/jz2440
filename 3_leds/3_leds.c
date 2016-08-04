b0059e8f511bf0cfff0dfe14a17418404e8b624f
sylar <hubin_jiang@htc.com>
2016-08-04 19:42:48 +0800

    裸板第三个程序，循环点亮3个led

---
+++ 3_leds/3_leds.c
@@ -0,0 +1,25 @@
+#define GPFCON (*(volatile unsigned long*)0x56000050)
+#define GPFDAT (*(volatile unsigned long*)0x56000054)
+
+#define GPF4_OUT (1<<8)
+#define GPF5_OUT (1<<10)
+#define GPF6_OUT (1<<12)
+
+void wait(volatile unsigned long dly){
+    int i=0;
+    for ( i = 0; i < dly; i++) {
+
+    }
+}
+int main(){
+    unsigned long i=0;
+    GPFCON = GPF4_OUT|GPF5_OUT |GPF6_OUT;
+    while(1){
+        wait(300000);
+        GPFDAT =  1<<i;
+        if(i++ > 8){
+            i=0;
+        }
+    }
+
+}
